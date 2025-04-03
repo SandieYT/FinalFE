25e524eff5cfbf8b66f1ee145c87033698eabcde
SandieYT <sandie594759@gmail.com>
2025-04-03 22:00:48 +0700

    a

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
-    <div>
+    <div className="root-div">
       <Header />
       <Outlet />
     </div>

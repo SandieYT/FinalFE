a6f2aafbef6b133d098451a1c1fba8c607c6b73b
WUANZ10 <nguyenquangmexio100@gmail.com>
2025-05-24 10:09:54 +0700

    update login gg

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,20 @@ import App from "./App";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TransactionProvider } from "./context/TransactionContext";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const queryClient = new QueryClient();
+const googleClientId = import.meta.env.VITE_GG_CLIENT_ID;
+console.log("Google Client ID:", googleClientId);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <TransactionProvider>
-        <App />
+        <GoogleOAuthProvider clientId={googleClientId}>
+          <App />
+        </GoogleOAuthProvider>
       </TransactionProvider>
     </QueryClientProvider>
   </Provider>

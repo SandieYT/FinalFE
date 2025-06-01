a608d5c41991c08919fbbd969f8d2170e16b15a1
WUANZ10 <nguyenquangmexio100@gmail.com>
2025-06-01 09:55:05 +0700

    1

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -126,7 +126,7 @@ export default function Login() {
         <div className="login-box">
           <h2 className="login-title">Login</h2>
 
-          <div className="google-login">
+          <div className="login-google">
             <GoogleLogin
               onSuccess={handleGoogleLoginSuccess}
               onError={() => toast.error("Google login failed.")}

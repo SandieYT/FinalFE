a1bdb7eafd7fca88ed7252fd526a097ef10728e2
SandieYT <sandie594759@gmail.com>
2025-06-06 19:11:47 +0700

    a

--- src/pages/ProfilePage/Profile.jsx
+++ src/pages/ProfilePage/Profile.jsx
@@ -132,7 +132,7 @@ export default function Profile() {
         formData.append('image', selectedImage);
       
         try {
-          toast.loading("Uploading profile picture...");
+          toast.loading("Uploading thumbnail...");
           const res = await axios.post(`${import.meta.env.VITE_API_URL}/upload/thumbnail`, formData);
           const imageUrl = res.data.url;
           await userService.updateUser(profile.userId, { thumbnail: imageUrl });
